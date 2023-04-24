ea563ead41fe0ae8dd61e22221c4f5864a8107ce
birahnu-moges <birhanu18@gmail.com>
2023-04-24 06:34:34 -0700

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,7 +16,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 1; i < size; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i; j++)
 		{
 			if (array[j] > array[j + 1] && array[j + 1])
 			{
